5bc4dc9881f24ae6d6ddeb37497f66be17d5d024
lloydcody <lloydcody@gmail.com>
2025-01-03 21:27:08 +1100

    Temp allow all CORS origins

--- server/index.js
+++ server/index.js
@@ -15,20 +15,27 @@ const app = express();
 setupSecurityHeaders(app);
 
 // Enable CORS with origin validation
-app.use(cors({
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     // Allow requests with no origin (like mobile apps or curl requests)
+//     if (!origin) return callback(null, true);
     
-    if (config.corsOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      logger.error('Not allowed by CORS', null, { origin });
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
+//     if (config.corsOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       logger.error('Not allowed by CORS', null, { origin });
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true
+// }));
+
+// temporarily allow all orignsi
+app.use(cors({
+  origin: '*',
+  credentials: false // Since we're allowing all origins, we should disable credentials
 }));
+
 
 // API routes
 app.use('/api/files', filesRouter);
